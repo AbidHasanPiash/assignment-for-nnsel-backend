e7715ed0910c510f3028f518d5c04bc415f54cd6
AbidHasanPiash <meghpiash@gmail.com>
2025-01-23 19:54:49 +0600

    feat: enable CORS support and add .env.example for configuration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS and allow your frontend (port 3000)
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN, // Allow requests from frontend
+    methods: process.env.CORS_METHODS, // Add allowed methods
+    allowedHeaders: process.env.CORS_HEADERS, // Add allowed headers
+  });
   app.setGlobalPrefix('api'); // Adds 'api' prefix to all routes
   await app.listen(process.env.PORT ?? 5000);
 }
